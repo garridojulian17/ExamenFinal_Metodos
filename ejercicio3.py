abf3aa11b8e4794fc4221dc4093b76a86b09cfdc
garridojulian17-enfinal_metodos-1vlaumn2 <garridojulian17-enfinal_metodos-1vlaumn2@local>
2018-05-25 01:08:25 +0000

    mensaje

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,6 +4,35 @@
 # Calcule la covarianza entre `u` y `v` e imprima su valor.
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+plt.figure()
+plt.plot(t,u)
+plt.plot(t,v)
+plt.ylabel("u,v")
+plt.xlabel("t")
+plt.savefig("serie.png")
+# covarianza
+def Covarianza(u,v):
+    Uprom= np.mean(u)
+    Vprom = np.mean(v)
+    for i in range(len(u)):
+        for j in range(len(v)):
+            cov = np.sum(((u[i]-Uprom)*(v[j]-Vprom))/(len(u)-1))
+            return cov
+        
+        
+        
+y=Covarianza(u,v)
+print (y)
+        
+        
+    
