a90d021f2e6e84cc24bd77a90dc94bd2eee80c28
garridojulian17-enfinal_metodos-1vlaumn2 <garridojulian17-enfinal_metodos-1vlaumn2@local>
2018-05-25 01:12:21 +0000

    mensaje

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,6 +5,16 @@ import numpy as np
 
 x = np.int_(np.random.random(100)*1000)
 print(x)
-
+impares=[]
+imprimir=[]
+for i in range(len(x)):
+    if(x[i]%2!=0):
+        impares.append(x[i])
+        if(impares[i]<800):
+            imprimir.append(impares[i])
+            i=i+1
+            print(imprimir)
+        
+        
 
 
